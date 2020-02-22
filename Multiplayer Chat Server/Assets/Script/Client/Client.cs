6dc857c0d1d790aff75ef353ee5d86729e0aeef0
cxz5309 <cxz5309@gmail.com>
2020-02-22 17:43:14 +0900

    no message

---
+++ Multiplayer Chat Server/Assets/Script/Client/Client.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    public GameObject chatContainer;
+    public GameObject messagePrefab;
+
+    public string clientName;
+
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectToServer()
+    {
+        //이미 연결되어있다면 이 함수를 무시
+        if (socketReady)
+            return;
+        //기본 호스트/포트 값
+        string host = "127.0.0.1";
+        int port = 6321;
+        //호스트/포트값 초기화(창 안에 무언가 쓰여져있다면)
+        string h;
+        int p;
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+            host = h;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+            port = p;
+
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if(data != null)
+                {
+                    OnIncomingData(data);
+                }
+            }
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        if(data == "%NAME")
+        {
+            Send("&NAME|" + clientName);
+            return;
+        }
+        GameObject go = Instantiate(messagePrefab, chatContainer.transform) as GameObject;
+        go.GetComponentInChildren<Text>().text = data;
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+
+    private void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+        writer.Close();
+        reader.Close();
+        socket.Close();
+        socketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
---
+++ Multiplayer Chat Server/Assets/Script/Server/Server.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectList;
+
+    public int port = 6321;
+    private TcpListener server;
+    private bool serverStarted;
+
+    private void Start()
+    {
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListeneng();
+            serverStarted = true;
+            Debug.Log("포트에서 서버가 시작되었습니다. " + port.ToString());
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (!serverStarted)
+            return;
+        foreach (ServerClient c in clients)
+        {
+            // 클라이언트가 아직 연결되어있는가
+            if (!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectList.Add(c);
+                continue;
+            }
+            //클라이언트로부터 메세지 체크
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < disconnectList.Count - 1; i++)
+        {
+            Broadcast(disconnectList[i].clientName + "의 연결이 끊어졌습니다!", clients);
+
+            clients.Remove(disconnectList[i]);
+            disconnectList.RemoveAt(i);
+        }
+    }
+
+    private void Broadcast(string data, List<ServerClient> cl)
+    {
+        foreach(ServerClient c in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush(); 
+            }
+            catch(Exception e)
+            {
+                Debug.Log("Write error : " + e.Message + " to client " + c.clientName);
+            }
+        }
+    }
+
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        if (data.Contains("&NAME"))
+        {
+            c.clientName = data.Split('|')[1];
+            Broadcast(c.clientName + "가 연결되었습니다!", clients);
+            return;
+        }
+
+        Broadcast(c.clientName + " : " + data, clients);
+    }
+
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+                return true;
+            }
+            else
+                return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    private void StartListeneng()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient, server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+        StartListeneng();
+
+        //누군가가 연결되었다고 모두에게 메세지를 보낸다.
+        Broadcast("%NAME", new List<ServerClient>() { clients[clients.Count - 1] });
+    }
+}
+
+public class ServerClient
+{
+    public TcpClient tcp;
+    public string clientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+        clientName = "Guest";
+        tcp = clientSocket;
+    }
+}
